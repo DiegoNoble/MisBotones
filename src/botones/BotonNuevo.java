79d2e5297cf82082de06e032cb26c248cfb19ae6
dnoble <dnoble@RIVWS007.manteo.local.uy>
2015-06-26 17:14:37 -0300

    MisBotones

---
+++ src/botones/BotonNuevo.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package botones;
+
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.event.MouseListener;
+
+/**
+ *
+ * @author dnoble
+ */
+public class BotonNuevo extends javax.swing.JLabel implements MouseListener {
+
+    private final String IMAGEN_NORMAL = "/imagenes/add_normal.png";
+    private final String IMAGEN_CHICA = "/imagenes/add_chico.png";
+    private final String IMAGEN_GRANDE = "/imagenes/add_grande.png";
+
+    public BotonNuevo() {
+        super();
+        //se le da un tamaño        
+        this.setPreferredSize(new Dimension(260, 60));
+        //se le coloca una imagen
+        this.setIcon(new javax.swing.ImageIcon(getClass().getResource(IMAGEN_NORMAL)));
+        //se cambia de cursor default por otro, el de la "manito"
+        this.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        //se añade los eventos del mouse
+        this.addMouseListener(this);
+        this.setFont(new java.awt.Font("Tahoma", 1, 12));
+        this.setBorder(null);
+        this.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        this.setIconTextGap(-3);
+        this.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        this.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+    }
+
+    @Override
+    public void mouseClicked(java.awt.event.MouseEvent me) {
+        this.setIcon(new javax.swing.ImageIcon(getClass().getResource(IMAGEN_GRANDE)));
+    }
+
+    @Override
+    public void mousePressed(java.awt.event.MouseEvent me) {
+        this.setIcon(new javax.swing.ImageIcon(getClass().getResource(IMAGEN_CHICA)));
+    }
+
+    @Override
+    public void mouseReleased(java.awt.event.MouseEvent me) {
+        this.setIcon(new javax.swing.ImageIcon(getClass().getResource(IMAGEN_NORMAL)));
+    }
+
+    @Override
+    public void mouseEntered(java.awt.event.MouseEvent me) {
+        this.setIcon(new javax.swing.ImageIcon(getClass().getResource(IMAGEN_GRANDE)));
+    }
+
+    @Override
+    public void mouseExited(java.awt.event.MouseEvent me) {
+        this.setIcon(new javax.swing.ImageIcon(getClass().getResource(IMAGEN_NORMAL)));
+    }
+}
